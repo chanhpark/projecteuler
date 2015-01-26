4f10e243492228238152a049ea9c9b1a6430cba5
chanhpark <chanhyun.park1211@gmail.com>
2015-01-26 14:17:41 -0500

    project euler problem 2

---
+++ euler2.rb
@@ -0,0 +1,19 @@
+def FibRecursive(n)
+  return 1 if n <= 2
+  return (FibRecursive n - 1) + (FibRecursive n - 2)
+end
+
+def FibEven(x)
+ sum = 0
+ a = 1
+ b = 2
+  while (b < x)
+    if ( b % 2 == 0 )
+      sum += b
+    end
+    a,b = b, a+b
+  end
+   return sum
+end
+
+puts "answer: #{FibEven(500)}"
